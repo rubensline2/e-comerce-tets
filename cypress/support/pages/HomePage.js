4ee4d7fa53064685aee8b2420a38f12228d5d1b5
Dell <rubensline@gmail.com>
2025-06-11 17:33:50 -0300

    Ajuste campo com id dinamico na homepage

--- cypress/support/pages/HomePage.js
+++ cypress/support/pages/HomePage.js
@@ -1,6 +1,5 @@
 export class HomePage {
   buscarProduto(produto) {
-    cy.get('#nav-bb-search').type(produto);
-    cy.get('.nav-bb-button').click();
+    cy.get('#twotabsearchtextbox').type(produto).type('{enter}');
   }
 }
